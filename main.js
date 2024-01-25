a285bb2dfb0960d6e07673d5951c773963736a01
artem6742 <144213709+artem6742@users.noreply.github.com>
2024-01-25 21:10:37 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,247 @@
+// задача 1
+
+// let a = prompt();
+// if (a == 10){
+//     console.log('верно')
+// }else{
+//     console.log('не верно')
+// }
+
+
+// Task 2
+
+// let min = prompt()
+//
+// if (min > 15){
+//     console.log('первая четверть')
+// }else if (15 < min > 30){
+//     console.log('вторая четверть ')
+// }else if (30 < min > 45){
+//     console.log('третья четверть ')
+// }else if (45< min > 60){
+//     console.log('третья четверть ')
+// }else{
+//     console.log('ввелите число от 1 до 60')
+// }
+
+// Task 3
+
+// let lang = prompt()
+// let arr;
+// if (lang === 'ru') {
+//     arr = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+// } else if (lang === 'en') {
+//     arr = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+// } else {
+//     arr = [];
+// }
+
+
+
+// Работа с if-else
+
+// let a = prompt()
+// if (a == 0) {
+//     console.log('true')
+// } else {
+//     console.log('false')
+// }
+//
+//
+//
+// let a = prompt()
+// if (a > 0) {
+//     console.log('true')
+// } else {
+//     console.log('false')
+// }
+//
+//
+// let a = prompt()
+// if (a < 0) {
+//     console.log('true')
+// } else {
+//     console.log('false')
+// }
+//
+//
+//
+// let a = prompt()
+// if (a >= 0) {
+//     console.log('true')
+// } else {
+//     console.log('false')
+// }
+//
+//
+// let a = prompt()
+// if (a <= 0) {
+//     console.log('true')
+// } else {
+//     console.log('false')
+// }
+//
+//
+// let a = prompt()
+// if (a != 0) {
+//     console.log('true')
+// } else {
+//     console.log('false')
+// }
+//
+// let a = prompt()
+// if (a == 'test') {
+//     console.log('true')
+// } else {
+//     console.log('false')
+// }
+//
+// let a = prompt()
+// if (a == '1') {
+//     console.log('true')
+// } else {
+//     console.log('false')
+// }
+//
+//
+
+// let a = prompt()
+// if (a > 0 && a < 5) {
+//     console.log('true')
+// } else {
+//     console.log('false')
+// }
+
+// let a = prompt()
+// if (a == 0 || a == 2) {
+//     console.log(a + 7)
+// } else {
+//     console.log(a / 10)
+// }
+//
+// let a = prompt()
+// if (a == 0) {
+//     console.log('true')
+// } else {
+//     console.log('false')
+// }
+
+// let a = prompt()
+// let b = prompt()
+// if (a <= 1 && b >= 3) {
+//     console.log(a + b)
+// } else {
+//     console.log(a - b)
+// }
+
+// let a = prompt()
+// let b = prompt()
+// if (a > 2 && a < 11 || b >= 3){
+//     console.log(a + b)
+// }else{
+//     console.log(a - b)
+// }
+
+// Задачи на Swith-case
+// let num = prompt()
+// switch (num){
+//     case '1':
+//         console.log('зима')
+//         break;
+//     case '2':
+//         console.log('весна')
+//         break;
+//     case '3':
+//         console.log('Осень')
+//         break;
+//     case '4':
+//         console.log('лето')
+//         break;
+//     default:
+//         console.log('введите от число 1 до 4')
+//
+// }
+
+// Task
+
+// let day = prompt();
+// if (day >= 10 ){
+//     console.log('первая декада')
+// }else if(day > 10 && day <20){
+//     console.log('вторая декада')
+// }else if(day > 20 && day <31){
+//     console.log('3 декада');
+//
+
+// let month = prompt()
+// switch (month){
+//     case 1 || 2 || 3:
+//         console.log('зима');
+//         break;
+//     case 4 || 5 || 6:
+//         console.log('лето')
+//         break;
+//     case 7 || 8 || 9:
+//         console.log('весна')
+//         break;
+//     case 10 || 11 || 12:
+//         console.log('осень')
+// }
+
+
+// let a = prompt()
+// if (a[0] == 'a'){
+//     console.log('yes')
+// }else {
+//     console.log('no')
+// }
+
+
+// let a = prompt();
+// if (a[0] == 1){
+//     console.log('yes')
+// }else{
+//     console.log('no')
+// }
+
+
+// let a = prompt()
+// let b = Number(a[0])
+// let c =Number(a[1])
+// let d =Number(a[2])
+// console.log(d + c + b)
+
+
+// let a = prompt()
+// let b = Number(a[0])
+// let c =Number(a[1])
+// let d =Number(a[2])
+// let e =Number(a[3])
+// let f =Number(a[4])
+// let g =Number(a[5])
+//
+// if (b + c + d === e + f + g){
+//     console.log('yes')
+// }else{
+//     console.log('no')
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
